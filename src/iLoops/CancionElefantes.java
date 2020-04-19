e582b50b0c07be62feb63f40966a3e719eb8398a
mfgg01 <mfgg01@yahoo.com.mx>
2020-04-18 20:59:59 -0500

    Elefantes, MarioFacil y MarioDificil

--- src/iLoops/CancionElefantes.java
+++ src/iLoops/CancionElefantes.java
@@ -3,10 +3,16 @@ package iLoops;
 public class CancionElefantes {
     public static void main(String[] args) {
         int numElefantes = 1;
-        while (numElefantes <= 100) {
-            System.out.println(numElefantes + " elefante(s) se columpiaba(n) sobre la tela de una araña,");
-            System.out.println("como veía(n) que resistia(n), fueron a llamar a otro elefante...");
-            numElefantes++;
+        int totalElefantes = 100;
+        while (numElefantes <= totalElefantes) {
+            if (numElefantes > 1) {
+                System.out.println("\n" + numElefantes + " elefantes se columpiaban sobre la tela de una araña,");
+                System.out.println("como veían que resistia, fueron a llamar a otro elefante...");
+            } else {
+        System.out.println("\n" + numElefantes + " elefante se columpiaba sobre la tela de una araña,");
+        System.out.println("como veía que resistia, fueron a llamar a otro elefante...");
+            }
+        numElefantes++;
         }
     }
 }
